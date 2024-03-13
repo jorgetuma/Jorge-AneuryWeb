a52a0a40f84a8146c08ba3eb0854153c5679a9bc
jorgetuma <jetm0001@ce.pucmm.edu.do>
2024-03-12 23:10:45 -0400

    ligeros cambios a la funcion CRUD Reserva

--- practica-7/src/main/java/com/grupo5/practica7/funciones/FuncionCRUDReserva.java
+++ practica-7/src/main/java/com/grupo5/practica7/funciones/FuncionCRUDReserva.java
@@ -1,4 +1,17 @@
 package com.grupo5.practica7.funciones;
 
-public class FuncionCRUDReserva {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.Gson;
+import com.grupo5.practica7.servicios.ReservaDynamoDbService;
+
+public class FuncionCRUDReserva implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    private final ReservaDynamoDbService reservaDynamoDbService = new ReservaDynamoDbService();
+    private final Gson gson = new Gson();
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
+        return null;
+    }
 }
