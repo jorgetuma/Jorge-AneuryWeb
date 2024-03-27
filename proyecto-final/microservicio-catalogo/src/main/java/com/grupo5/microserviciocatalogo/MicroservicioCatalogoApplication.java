b90499621a2c4e4f0a74c231b3080055eb554aaa
jorgetuma <jetm0001@ce.pucmm.edu.do>
2024-03-26 22:45:15 -0400

    Proyecto microservicio para catalogo de libros

---
+++ proyecto-final/microservicio-catalogo/src/main/java/com/grupo5/microserviciocatalogo/MicroservicioCatalogoApplication.java
@@ -0,0 +1,13 @@
+package com.grupo5.microserviciocatalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioCatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioCatalogoApplication.class, args);
+	}
+
+}
---
+++ proyecto-final/microservicio-catalogo/src/test/java/com/grupo5/microserviciocatalogo/MicroservicioCatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo5.microserviciocatalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioCatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
