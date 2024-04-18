6c69a2220de9cf409ba2380052e0e68daaddc0bf
AneuryEstevez <87092561+AneuryEstevez@users.noreply.github.com>
2024-04-17 20:50:34 -0400

    servicios configuración y eureka

---
+++ proyecto-final/servidor-configuracion/src/test/java/com/grupo5/servidorconfiguracion/ServidorConfiguracionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo5.servidorconfiguracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorConfiguracionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ proyecto-final/servidor-eureka/src/test/java/com/grupo5/servidoreureka/ServidorEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo5.servidoreureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
